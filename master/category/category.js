4c877a775facebe1f5bca42876446aa9ead4f9b3
followpraveen <seotrial2000@gmail.com>
2021-04-23 13:54:18 +0530

    Integration in progress

--- master/category/category.js
+++ master/category/category.js
@@ -17,20 +17,20 @@ myApp.config(['$routeProvider', function ($routeProvider) {
 
 myApp.controller('categoryCtrl', ['$scope', 'categoryService', function ($scope, categoryService) {
 
-  ////////////////////////////////////// language section ///////////////////////////////////////////////////////
+  ////////////////////////////////////// catsuage section ///////////////////////////////////////////////////////
 
   ////////////////pagination////////////////////
 
 
   $scope.pageSize = 2;
   $scope.pageIndex = 0;
-  // $scope.maxSize=6;
-  // $scope.totalItemsL=0;
-  // $scope.numPagesL="";
-  // // $scope.pagesizeSelected="";
+  $scope.maxSize = 6;
+  $scope.totalItemsC = 0;
+  $scope.numPagesC = "";
+  // $scope.pagesizeSelected="";
 
 
-  $scope.cat = {
+  $scope.cats = {
     categoryName: "",
     categoryCode: "",
     description: "",
@@ -51,13 +51,86 @@ myApp.controller('categoryCtrl', ['$scope', 'categoryService', function ($scope,
           categoryId: value.categoryId, categoryName: value.categoryName, categoryCode: value.categoryCode, description: value.description,
           createdDate: value.insertedDate, updatedDate: value.updatedDate, createdBy: value.createdBy.userName
         });
-        $scope.totalItemsL = response.data.totalElements;
-        $scope.numPagesL = response.data.totalPages;
+        $scope.totalItemsC = response.data.totalElements;
+        $scope.numPagesC = response.data.totalPages;
       });
     });
   }
 
+  $scope.changePageSize = function () {
+    $scope.pageIndex = 0;
+    getallCat();
+  }
 
+  $scope.pageChange = function () {
+    $scope.pageIndex = $scope.pageIndex - 1;
+    getallCat($scope.pageIndex, $scope.pageSize);
+    $scope.pageIndex = $scope.pageIndex + 1;
+  }
+
+
+  ///////////////end of pagination///////////////////
+  $scope.cats = {
+    createdBy: {
+      userId: usId
+    },
+    updatedBy: {
+      userId: usId
+    },
+    description: "",
+    categoryCode: "",
+    categoryName: "",
+    categoryId: ""
+  };
+
+  $scope.editcats = function (x) {
+    $scope.cats.categoryName = x.categoryName;
+    $scope.cats.description = x.description;
+    $scope.cats.categoryId = x.categoryId;
+    $scope.cats.categoryCode = x.categoryCode;
+    console.log($scope.cats);
+  }
+  $scope.deleteLang = function () {
+
+    Swal.fire({
+      title: "Are you sure?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        appService.deleteLang($scope.lang).then(function () {
+          $scope.pageChange();
+          Swal.fire("Deleted!");
+        });
+        $scope.loadLanguage();
+      }
+    });
+
+
+  }
+  $scope.clearcats = function () {
+    $scope.cats.categoryName = "";
+    $scope.cats.description = "";
+    $scope.cats.categoryId = "";
+    $scope.cats.categoryCode = "";
+    console.log($scope.cats);
+  }
+  $scope.addcats = function () {
+    appService.addcats($scope.cats).then(function (response) {
+      $scope.pageChange();
+      if (response.data.responseCode == 201) {
+        swal("Done!", "", "success");
+      }
+      else {
+        swal("Failed!", "", "warning");
+      }
+      $scope.loadCategory();
+    });
+
+  }
 
 
 
@@ -66,12 +139,12 @@ myApp.controller('categoryCtrl', ['$scope', 'categoryService', function ($scope,
     $scope.catpop = false;
   }
 
-  $scope.addcate = function () {
-    $scope.catpop = $scope.addcatbtn = true; $scope.catedit = false
+  $scope.addcat = function () {
+    $scope.catpop = $scope.addcatbtn = true; $scope.catedit = $scope.tabShow = catdelete = false;
   }
 
-  $scope.loadCat = function () {
-    $scope.catpop = false
+  $scope.loadCategory = function () {
+    $scope.tabShow = true; $scope.catpop = false;
   }
 
 }]);
--- master/category/categoryService.js
+++ master/category/categoryService.js
@@ -1,45 +1,38 @@
 myApp.service('categoryService', ['$http', function ($http) {
 
+    const URL = "https://executivetracking.cloudjiffy.net/ExecutiveTrackingWeb/";
 
+    this.addcats = function (cats) {
+        if (cats.categoryId == 0 || cats.categoryId == null) {
+            return $http({
+                method: "POST",
+                url: URL + "category/v1/createCategory",
+                data: JSON.stringify(cats)
+            })
+        }
+        else {
+            return $http({
+                method: "PUT",
+                url: URL + "category/v1/updateCategory",
+                data: JSON.stringify(cats)
+            })
+        }
+    }
 
     this.getallCat = function (pageIndex, pageSize) {
         return $http({
             method: 'GET',
-            url: "https://executivetracking.cloudjiffy.net/ExecutiveTrackingWeb/category/v1/getAllCategoryByPagination/{pageNumber}/{pageSize}?pageNumber=" + pageIndex + "&pageSize=" + pageSize
+            url: URL + "category/v1/getAllCategoryByPagination/{pageNumber}/{pageSize}?pageNumber=" + pageIndex + "&pageSize=" + pageSize
         });
+    }
+
+    this.deletecats = function (cats) {
+        return $http({
+            method: "DELETE",
+            url: URL + "category/v1/deleteCategoryByCategoryId/" + cats.categoryId
+        })
     }
 
 
 
-
-    // ///////////////////////////////////////////language service///////////////////////////////////////////////////
-    //     this.addCat=function(cat){
-    //         if(lang.languageId==0 || lang.languageId==null){
-    //          return $http({
-    //               method: "POST",
-    //               url:"https://raghunkadur.cloudjiffy.net/RaghuNKadurWeb/language/v1/createLanguage",
-    //               data: JSON.stringify(cat)
-    //          })
-    //         }
-    //         else{
-    //             return $http({
-    //                 method: "PUT",
-    //                 url:"https://raghunkadur.cloudjiffy.net/RaghuNKadurWeb/language/v1/updateLanguage",
-    //                 data: JSON.stringify(cat)
-    //            }) 
-    //         }
-    //     }
-    //     this.getallCats = function(pageIndex,pageSize){
-    //         return $http({
-    //             method: 'GET',
-    //             url:"https://raghunkadur.cloudjiffy.net/RaghuNKadurWeb/language/v1/getAllLanguageByPagination/{pageNumber}/{pageSize}?pageNumber="+pageIndex+"&pageSize="+pageSize
-    //         });
-    //     }
-    //     this.deleteCat = function(cat){
-    //         return $http({
-    //             method: "DELETE",
-    //             url:"https://raghunkadur.cloudjiffy.net/RaghuNKadurWeb/language/v1/deleteLanguageById/"+cat.languageId
-    //         })
-    //     }
-    // // //////////////////////////////////////end of language service/////////////////////////////////////////////
 }]);
